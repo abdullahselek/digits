9df8610fee3137eb8d253ad246e37693229a0b32
Abdullah Selek <abdullahselek@gmail.com>
2018-04-10 20:57:37 +0100

    Function for random image datas.

--- digits/data_handler.py
+++ digits/data_handler.py
@@ -5,6 +5,14 @@ from __future__ import division
 import os
 import scipy.io as sio
 
+from random import randint
+
 def load_mat(data_dir, mat_file):
     path = os.path.join(data_dir, mat_file)
     return sio.loadmat(path)
+
+def random_img_datas(mat_dict):
+    X = mat_dict['X']
+    m = len(X)
+    rnd_indices = [randint(0, m) for _ in range(0, 100)]
+    return map(X.__getitem__, rnd_indices)
--- tests/test_data_handler.py
+++ tests/test_data_handler.py
@@ -2,10 +2,17 @@
 
 import os.path
 import unittest
+import os
+
 from digits import data_handler
 
 class DataHandlerTest(unittest.TestCase):
 
+    training_data = data_handler.load_mat('testdata', 'handwritten-digits.mat')
+
     def test_load_mat(self):
-        training_data = data_handler.load_mat('testdata', 'handwritten-digits.mat')
-        self.assertIsNotNone(training_data)
+        self.assertIsNotNone(self.training_data)
+
+    def test_random_img_datas(self):
+        datas = data_handler.random_img_datas(self.training_data)
+        self.assertEqual(len(list(datas)), 100)

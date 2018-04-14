466efd73a28317dcc80fcda7f03cb801138b42a3
Abdullah Selek <abdullahselek@gmail.com>
2018-04-14 01:02:03 +0100

    Use matplot's ImageGrid to display datas.

--- digits/data_handler.py
+++ digits/data_handler.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import math
 
 from random import randint
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 def load_mat(data_dir, mat_file):
     path = os.path.join(data_dir, mat_file)
@@ -21,38 +22,14 @@ def random_img_datas(mat_dict):
     return list(map(X.__getitem__, rnd_indices))
 
 def display_data(data_list):
-    width = int(round(math.sqrt(np.size(data_list, axis=1))))
-    # Gray image
-    im = plt.imshow(data_list,
-                    interpolation='bilinear',
-                    origin='lower',
-                    extent=(-3,3,-3,3))
-    # Compute rows, cols
-    m, n = np.shape(data_list)
-    print(m)
-    height = int(n / width)
-    # Compute number of items to display
-    display_rows = int(math.floor(math.sqrt(m)))
-    display_cols = int(math.ceil(m / display_rows))
-    print(display_rows, display_cols)
-    # Between images padding
-    pad = 1
-    # Setup blank display
-    display_array = np.ones((pad + display_rows * (height + pad),
-                            pad + display_cols * (width + pad)))
-    print(display_array)
-    # Copy each example into a patch on the display array
-    curr_ex = 0
-    for j in range(0, display_rows):
-        for i in range(0, display_cols):
-            print(curr_ex)
-            if curr_ex > m - 1:
-                break
-            #  Copy and get the max value of the patch
-            max_val = np.max(np.abs(data_list[curr_ex]))
-            display_array[pad + (j - 1) * (height + pad) + np.arange(1, height),
-                          pad + (i - 1) * (width + pad) + np.arange(1, width)] = np.reshape(data_list[curr_ex], (height, width)) / max_val
-            curr_ex += 1
-        if curr_ex > m - 1:
-            break
+    fig = plt.figure(1, (20, 20))
+    grid = ImageGrid(fig,
+                     141,
+                     nrows_ncols=(10, 10))
+
+    for i in range(100):
+        img_data = np.reshape(data_list[i], (40, 10))
+        _ = grid[i].imshow(img_data, interpolation="nearest")
+
+    plt.draw()
     plt.show()

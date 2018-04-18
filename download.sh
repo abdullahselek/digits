9e0c2401e8534a5fe440bc2e39a13adb960c4f65
Abdullah Selek <abdullahselek@gmail.com>
2018-04-18 23:25:54 +0100

    Shell scripts which downloads all mnist datasets.

---
+++ download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd "digits"
+DATASET="dataset"
+rm -rf "$DATASET" 
+mkdir "$DATASET"
+cd "$DATASET"
+
+URL="http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
+echo "Downloading from $URL"
+curl "$URL" --output train-images-idx3-ubyte.gz
+echo "train-images-idx3-ubyte.gz downloaded"
+
+URL="http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
+echo "Downloading from $URL"
+curl "$URL" --output train-labels-idx1-ubyte.gz
+echo "train-labels-idx1-ubyte.gz downloaded"
+
+URL="http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
+echo "Downloading from $URL"
+curl "$URL" --output t10k-images-idx3-ubyte.gz
+echo "t10k-images-idx3-ubyte.gz downloaded"
+
+URL="http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
+echo "Downloading from $URL"
+curl "$URL" --output t10k-labels-idx1-ubyte.gz
+echo "t10k-labels-idx1-ubyte.gz downloaded"
+
+# unzip all zippd files
+gzip -d *.gz
+rm -rf *.gz

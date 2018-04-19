45b1af35f5c07fe4eebfe9bbcf7d65f9384a1939
Abdullah Selek <abdullahselek@gmail.com>
2018-04-19 11:03:50 +0100

    Extract file name from url.

--- download.sh
+++ download.sh
@@ -8,23 +8,23 @@ cd "$DATASET"
 
 URL="http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
 echo "Downloading from $URL"
-curl "$URL" --output train-images-idx3-ubyte.gz
-echo "train-images-idx3-ubyte.gz downloaded"
+curl "$URL" --output "${URL##*/}"
+echo "${URL##*/} downloaded"
 
 URL="http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
 echo "Downloading from $URL"
-curl "$URL" --output train-labels-idx1-ubyte.gz
-echo "train-labels-idx1-ubyte.gz downloaded"
+curl "$URL" --output "${URL##*/}"
+echo "${URL##*/} downloaded"
 
 URL="http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
 echo "Downloading from $URL"
-curl "$URL" --output t10k-images-idx3-ubyte.gz
-echo "t10k-images-idx3-ubyte.gz downloaded"
+curl "$URL" --output "${URL##*/}"
+echo "${URL##*/} downloaded"
 
 URL="http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
 echo "Downloading from $URL"
-curl "$URL" --output t10k-labels-idx1-ubyte.gz
-echo "t10k-labels-idx1-ubyte.gz downloaded"
+curl "$URL" --output "${URL##*/}"
+echo "${URL##*/} downloaded"
 
 # unzip all zipped files
 gzip -d *.gz
